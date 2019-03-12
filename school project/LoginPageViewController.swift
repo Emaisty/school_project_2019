c1f311ef7d8c4430fa80ab930e3c3287a8e8eb11
Emaisty <maxgusevfff@yandex.ru>
2019-03-13 00:57:25 +0300

    add login page

--- school project/LoginPageViewController.swift
+++ school project/LoginPageViewController.swift
@@ -2,7 +2,7 @@
 //  LoginPageViewController.swift
 //  school project
 //
-//  Created by Максим Гусев on 15/02/2019.
+//  Created by Максим Гусев on 12/03/2019.
 //  Copyright © 2019 Максим Гусев. All rights reserved.
 //
 
@@ -10,33 +10,31 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
 
-    @IBOutlet weak var emailusertextfield: UITextField!
-    @IBOutlet weak var passwordusertextfield: UITextField!
+    @IBOutlet weak var UserEmailTextField: UITextField!
+    @IBOutlet weak var UserPasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    @IBAction func loginaction(_ sender: Any) {
-        //here, must be auth
-        if emailusertextfield.text == "max-gusev@mail.ru" && passwordusertextfield.text == "Samoloff123" || emailusertextfield.text == "1" && passwordusertextfield.text == "12"{ self.performSegue(withIdentifier: "loginToHome", sender: self)
-        }else {
-            let alertController = UIAlertController(title: "User Incorrect", message: "enter valid data", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            
-            alertController.addAction(defaultAction)
-            self.present(alertController, animated: true, completion: nil)
-        }}/*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func EnterButton(_ sender: AnyObject) {
+        let UserEmail =  UserEmailTextField.text;
+        let UserPassword = UserPasswordTextField.text;
+        if (UserEmail!.isEmpty || UserPassword!.isEmpty){
+            displayMyAlertMessage(userMessage: "Enter all fields!");
+            return;
+        }
+        
+        self.performSegue(withIdentifier: "loginToHome", sender: self)
     }
-    */
+    
+    func displayMyAlertMessage(userMessage:String) {
+        var myAlert = UIAlertController(title: "Warning", message: userMessage, preferredStyle: UIAlertController.Style.alert);
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        self.present(myAlert,animated: true,completion: nil)
+    }
 
 }
-
